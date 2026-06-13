38870495fe01dfab19c0f34f3e210296ee865fc3
Koray Akdere <167217794+Koray-Akdere@users.noreply.github.com>
2026-06-13 12:08:18 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -71,7 +71,6 @@ app.post("/upload", upload.array("media", 50), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda aktif.`);
 });
